baf66909d627b565c8855bb31d33fea45558fe21
DESKTOP-F4BC13L\hi <wjdwn3132@naver.com>
2024-05-29 11:18:47 +0900

    [feat] erm, DDL, insert(grade, order_detail)
    추가

---
+++ src/main/webapp/DataBase/Insert.sql
@@ -0,0 +1,12 @@
+-grade-
+insert into grade values(0, 'Bronze', 1);
+insert into grade values(0, 'Silver', 2);
+insert into grade values(0, 'Gold', 3);
+insert into grade values(0, 'Platinum', 4);
+insert into grade values(0, 'Diamond', 5);
+
+-order_status-
+insert into order_status values(0, '주문완료');
+insert into order_status values(0, '배송준비중');
+insert into order_status values(0, '배송중');
+insert into order_status values(0, '배송완료');
