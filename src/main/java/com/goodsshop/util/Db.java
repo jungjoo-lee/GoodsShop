7303ab40662a3c7ec9e059b5def35cbbf916bfb5
DESKTOP-F4BC13L\hi <wjdwn3132@naver.com>
2024-05-30 09:52:42 +0900

    sadfasdf

--- src/main/java/com/goodsshop/util/Db.java
+++ src/main/java/com/goodsshop/util/Db.java
@@ -7,30 +7,36 @@ import java.sql.SQLException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import com.goodsshop.properties.Env;
+
 public class DB {
-		
+
 	public static Connection getConnection() {
 		Connection con = null;
 		try {
 			Context initContext = new InitialContext();
-			Context envContext = (Context)initContext.lookup("java:/comp/env");
-			DataSource ds = (DataSource)envContext.lookup("jdbc/MysqlDB");
+			Context envContext = (Context) initContext.lookup(Env.getEnvContext());
+			DataSource ds = (DataSource) envContext.lookup(Env.getDataSource());
 			con = ds.getConnection();
-		}catch (NamingException e) {e.printStackTrace();
-		}catch (SQLException e) {e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		
 		return con;
 	}
-	
+
 	public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
 		try {
-			if(con!=null) con.close();
-			if(pstmt!=null) pstmt.close();
-			if(rs!=null) rs.close();
-		}catch (SQLException e) {e.printStackTrace();}
+			if (rs != null)
+				rs.close();
+			if (pstmt != null)
+				pstmt.close();
+			if (con != null)
+				con.close();	
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+	}
 }
-
